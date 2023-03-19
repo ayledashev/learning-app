16683f95b9821813f1177d5f6ebbe8ad530e449a
Andrew Ledashcev <ledashev.andrew@yandex.ru>
2023-03-19 21:35:59 +0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'learning-app';
+export class AppComponent implements OnInit {
+  biba = 'Biba';
+  
+  ngOnInit() {
+    let a = 2;
+    let b = 'Name';
+    
+    console.log(this.biba);
+    this.checkNumbers(a, b);
+  }
+
+  checkNumbers(par: number, parr: any) {
+    this.biba = 'JOPA';
+    if (par === parr) {
+      console.log('ДА');
+      console.log(this.biba);
+    } else {
+      console.log('НЕТ');
+      console.log(this.biba);
+    }
+  }
 }
