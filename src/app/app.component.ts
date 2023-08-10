7abde7548b23a39831b88fee987c0f58d504ee3f
Andrew Ledashcev <ledashev.andrew@yandex.ru>
2023-08-10 21:45:34 +0300

    clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,36 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  sumNumberOne = 232;
-  sumNumberTwo: any;
-  
+
+  constructor() {
+
+  }
+
   ngOnInit(): void {
-    this.sumNumberTwo = '500';
-    let stringCheck = this.sumNumberTwo + ': NUmber';
-    console.log(stringCheck);
 
-    if (typeof this.sumNumberTwo === 'number') {
-      this.checkNumbers(this.sumNumberOne, this.sumNumberTwo);
-    } else {
-      console.log('NE CHISLO');
-      let newNum = this.makeNumberPlusOne(this.sumNumberTwo);
-      console.log('NOVOE CHISLO', newNum);
-      this.checkNumbers(this.sumNumberOne, newNum);
-    }
-  }
-
-  checkNumbers(num1: number, num2: number) {
-    if (num1 == num2) {
-      console.log('РАВНЫ');
-    } else {
-      console.log('НЕТ');
-    }
-  }
-
-  makeNumberPlusOne(par: any): number {
-    par = +par;
-    par = par + 1;
-    console.log('DELAY CHISLO', par);
-    return par;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule }   from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
